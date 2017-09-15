cfb19ec6f3f29c98d85043e72263da90861c36bc
astankovic <aleksa.stankovic@berkeley.edu>
2017-09-15 11:39:14 +0200

    Remove material icons

--- client/components/Header/Header.jsx
+++ client/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 /* eslint no-unused-vars: 0 */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AccIcon from '../Decorative/AccIcon/AccIcon';
 
 import header from './header.scss';
-import icons from '../../common-styles/material-icons.scss';
 
 const Header = () => (
   <header styleName="header.header">
@@ -12,7 +12,7 @@ const Header = () => (
     </Link>
 
     <Link to="/dashboard">
-      <i styleName="icons.material-icons">account_box</i>
+      <AccIcon />
     </Link>
   </header>
 );
